7a8037fa4dbb751a7d9befc57412560620d974c4
Sophia C <schi60@stuy.edu>
2026-06-11 02:32:37 -0400

    updated calender css

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,9 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+if not app.secret_key:
+    raise RuntimeError("SECRET_KEY not found")
+	
 oauth = OAuth(app)
 
 google = oauth.register(
